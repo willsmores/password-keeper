b20fcfcfb2d437b602d006d2e4b7a8ea737f7f49
CerealJim <jim.haz34@gmail.com>
2024-02-01 12:26:21 -0500

    new authorization page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+export default function AuthRoute() {
+  return (
+    <div>
+      <h2>Please Sign In</h2>
+      <div>
+        <button>Login with Github</button>
+      </div>
+    </div>
+  );
+}
