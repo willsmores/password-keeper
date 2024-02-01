c4cb81d39fa544c9898d6cdbb81a0ecac889fc8d
CerealJim <jim.haz34@gmail.com>
2024-02-01 12:46:03 -0500

    github auth component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,11 @@
+import SignInWithGithub from "@/components/SignInWithGithub";
+
 export default function AuthRoute() {
   return (
     <div>
       <h2>Please Sign In</h2>
       <div>
-        <button>Login with Github</button>
+        <SignInWithGithub />
       </div>
     </div>
   );
---
+++ src/components/SignInWithGithub.tsx
@@ -0,0 +1,15 @@
+"use Client";
+
+import { signIn } from "next-auth/react";
+
+export default function SignInWithGithub() {
+  return (
+    <button
+      onClick={() =>
+        signIn("github", { callbackUrl: `${window.location.origin}` })
+      }
+    >
+      Login with Github
+    </button>
+  );
+}
